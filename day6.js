2fee0c53179807cba516d845fdde814b6ba84eda
Armoni Tigner <atigner2@student.gsu.edu>
2022-01-24 23:09:53 -0500

    inital cpmmit, needto do part 4-5

---
+++ day6.js
@@ -0,0 +1,25 @@
+https://exercism.org/tracks/javascript/exercises/elyses-destructured-enchantments
+
+// This function returns the first card in a deck 
+//without using array[x]or .shift
+function getFirstCard(deck){
+    let cardOne = deck.slice(0,1)
+    return cardOne
+}
+console.log(getFirstCard([5, 9, 7, 1, 8])) 
+// This function returns the second card in a deck 
+//without using array[x]or .shift
+function getSecondCard(deck){
+    let cardOne = deck.slice(1,2)
+    return cardOne
+}
+console.log(getSecondCard([5, 9, 7, 1, 8]))
+
+// This function returns the deck with 
+//the first two cards swapped
+
+function swapTopTwoCards(deck){
+
+    return deck
+}
+console.log
